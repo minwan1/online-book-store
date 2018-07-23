c658ae46e51a936515611a85e9c2c4e0a6b7b5ef
minwan <ansatgol@gmail.com>
2018-07-24 02:00:05 +0900

    md파일 수정

--- src/test/java/com/book/member/MemberControllerTest.java
+++ src/test/java/com/book/member/MemberControllerTest.java
@@ -10,7 +10,28 @@ public class MemberControllerTest {
     @Test
     public void name() {
 
+        final int convert = convert("123");
+        System.out.println(convert);
 
     }
 
+
+    public static int convert(String s) {
+
+        char[] ca = s.toCharArray();
+        /*
+        123
+        1 * 10 + 2 = 12
+        12 * 10 + 3= 123;
+         */
+        int num = 0;
+
+        for (char c : ca) {
+            num = num * 10;
+            num = num + (c - '0');
+        }
+
+        return num;
+
+    }
 }
