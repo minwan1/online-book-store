97c6adf0b27dd0e9dd130f0bfea7ab65b2e9b7b4
minwan <ansatgol@gmail.com>
2018-07-24 00:03:20 +0900

    fix content subject

--- src/main/java/com/book/member/domain/Email.java
+++ src/main/java/com/book/member/domain/Email.java
@@ -13,7 +13,6 @@ import javax.persistence.Embeddable;
 @Embeddable
 @Getter
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
-@JsonIgnoreProperties({"host", "id"})
 public class Email {
 
     //모든데서 다체크해야하는 이슈발생.
@@ -22,17 +21,4 @@ public class Email {
     @Column(name = "email", nullable = false, unique = true)
     private String value;
 
-    public Email(String value) {
-        this.value = value;
-    }
-
-    public String getHost() {
-        int index = value.indexOf("@");
-        return value.substring(index);
-    }
-
-    public String getId() {
-        int index = value.indexOf("@");
-        return value.substring(0, index);
-    }
 }
