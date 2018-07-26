f1a49258bbc1d645d25ec388359303edee5116ef
minwan <ansatgol@gmail.com>
2018-07-27 02:37:36 +0900

    H2 db 추가

---
+++ src/main/java/com/book/config/H2Config.java
@@ -0,0 +1,22 @@
+package com.book.config;
+
+import org.h2.tools.Server;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+@Profile("local")
+public class H2Config {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource") // yml의 설정값을 Set한다.
+    public DataSource dataSource() throws SQLException {
+        Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
+        return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
+}
